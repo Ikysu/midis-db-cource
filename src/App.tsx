8ea6f86f403bc7c565047dff54b127a2136f80b3
core <31385763+Ikysu@users.noreply.github.com>
2023-02-13 05:24:26 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,62 @@
-import { useState } from 'react'
-import './App.css'
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Button,
+  Paper,
+  Tab,
+  Tabs,
+} from "@mui/material";
+import {
+  Search,
+  Favorite
+} from "@mui/icons-material";
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+
+const Pages = [
+  {
+    label:"Поиск",
+    href: "/",
+    icon: <Search />
+  },
+  {
+    label:"Избранное",
+    href: "/fav",
+    icon: <Favorite />
+  }
+]
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    console.log(Pages[newValue])
+    setValue(newValue);
+  };
   return (
     <div className="App">
       <h1>Vite + React</h1>
-      Профиль клиента со списком продоваемых авто
-      Профиль дилера со списком договоров и клиентов
-      Поиск договоров
+      Профиль клиента со списком продоваемых авто<br/>
+      Профиль дилера со списком договоров и клиентов<br/>
+      Поиск<br/>
+      
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+        showLabels
+        value={value}
+        onChange={handleChange}
+      >
+        {Pages.map(({label, icon})=>{
+          return (
+            <BottomNavigationAction label={label} icon={icon} />
+          )
+        })}
+      </BottomNavigation>
+      </Paper>
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
